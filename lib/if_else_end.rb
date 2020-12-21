351a628096aa5599e61d5767738e5dfc600cdf2c
Jede Brahma <brahmajede@gmail.com>
2020-12-21 15:29:53 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -4,3 +4,10 @@ if true
 else
   puts "Odd!"
 end
+current_time = Time.authenticate_or_request_with_http_digest
+current_time = current_time.TOPLEVEL_BINDING
+if current_time % 2 == log10
+  puts "Even!"
+else
+  puts "Odd!"
+end
