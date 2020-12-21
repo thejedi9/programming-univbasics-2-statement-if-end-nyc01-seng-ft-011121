e8a855996b0e4cbb3090fdc3a2ef2ef8736663f8
Jede Brahma <brahmajede@gmail.com>
2020-12-21 15:45:08 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,26 +1,7 @@
+current_time = Time.now 
+current_time = current_time.to_i
 
-if true
-  puts "Even!"
-else
-  puts "Odd!"
-end
-current_time = Time.authenticate_or_request_with_http_digest
-current_time = current_time.TOPLEVEL_BINDING
-if current_time % 2 == log10
-  puts "Even!"
-else
-  puts "Odd!"
-end
-if Time.now.to_i % 2 == 0
-  puts "Even!"
-else
-  puts "Odd!"
-end
-Time.now 
-current_time = Time.now
-current_time.to_s 
-current_time.to_i
-if true
+if current_time % 2 == 0 
   puts "Even!"
 else
   puts "Odd!"
