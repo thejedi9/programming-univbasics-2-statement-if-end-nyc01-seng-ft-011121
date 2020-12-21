23346ddee36e3b23a7e3736f40aec26605637518
Jede Brahma <brahmajede@gmail.com>
2020-12-21 15:27:00 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,16 +1,6 @@
-#```ruby
-run_code_inside = true
-puts "Code before if...end"
-if run_code_inside
-  puts "code inside"
-end
-puts "Code after if...end"
-``` 
-time.authenticate_or_request_with_http_digest
-current time = Time.authenticate_or_request_with_http_digest
+
 if true
   puts "Even!"
 else
   puts "Odd!"
 end
-Write your solution here
